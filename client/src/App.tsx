a3e7c027b1a0ac7648a6831a9354da4238b19279
Thomas <thomasmartin98@hotmail.com>
2021-07-09 21:50:37 +0100

    working on server code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Movie from './components/movie-card/Movie';
 import movieJson from './films.json'
 import MoviePage from './components/movie-page/Movie-page'
+import axios from 'axios'
 
+import firebase from 'firebase';
+import 'firebase/firestore'
+
+
+import {DocumentDataHook, useCollectionData} from 'react-firebase-hooks/firestore'
+
+
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyBgvVrb5519-fykonbZOKeAdwq5e_JXpO4",
+//   authDomain: "movie-reviews-b76cf.firebaseapp.com",
+//   databaseURL: "https://movie-reviews-b76cf-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "movie-reviews-b76cf",
+//   storageBucket: "movie-reviews-b76cf.appspot.com",
+//   messagingSenderId: "401773917647",
+//   appId: "1:401773917647:web:d73143c850d70aa45aceba",
+//   measurementId: "G-1PYYDQBTPJ"
+
+// })
+//const firestore=firebase.firestore()
+
+//const movieDB:{id:string,data:any}[]=[]
+
+//const [movies ,setmovies]=useState([])
+
+// const fetchMovies=async () => {
+//   const res=firestore.collection("movies")
+//   const data=await res.get()
+//   data.docs.forEach(item=>{
+//     setmovies([...movies,item])
+//   })
+  
+// }
+
+
+
+///console.log(movieDB);
 
 type movieItems={
   
@@ -18,19 +56,42 @@ type movieItems={
 const filmJson:string=JSON.stringify(movieJson)
 let data=JSON.parse(filmJson)
 
-console.log(data)
+//console.log(data)
 
 export const App:React.FC=()=>{
+  const [movieData,SetmoveieData]=useState([])
+
+  const FetchMovieData=()=>{
+    console.log("Fetching movie data");
+    axios({
+      method: 'get',
+      url: 'http://127.0.0.1:8000/API/movieData'
+  }).then((res)=>{
+    SetmoveieData(res.data)
+    console.log(res.data)
+  })
+}
+
+  //On page load fetch the movie data
+  useEffect(() => {
+    FetchMovieData()
+  }, [])
+
+
   return (
     <div className="App">
       <MoviePage name={data[0].name}rating={data[0].rating} synopsis={data[0].SynopsisFull} imgsrc={data[0].imgsrc}/>
   
       <div className="movies-container">
-        {data.map((list:movieItems)=>
-            <Movie name={list.name}rating={list.rating} synopsis={list.Synopsis} imgsrc={list.imgsrc}/>
+        {movieData.map((list:movieItems)=>
+            <Movie key={list.id} name={list.name}rating={list.rating} synopsis={list.Synopsis} imgsrc={list.imgsrc}/>
 
         )}
+
+        
       </div>
+
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,33 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors'
+//import movieData from './static/movieData.json'
+const app = express()
+const port = 8000
+
+import fs from 'fs'
+
+let rawdata = fs.readFileSync('./static/movieData.json')
+let data = JSON.parse(rawdata);
 
 
-const app=express();
+app.use(cors())
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/API/movieData/:name?',(req,res)=>{
+    if (req.params.name==="LUCA"){
+        console.log("luca")
+    }
+})
+
+app.get('/API/movieData', (req, res) => {
+  res.json(data)
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
