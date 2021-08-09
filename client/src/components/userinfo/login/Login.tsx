1d507cb74010fb3c152fdfde16e56027777fe99c
Thomas <thomasmartin98@hotmail.com>
2021-08-09 20:08:06 +0100

    Added a few CSS changes

---
+++ client/src/components/userinfo/login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+            <h1 className="greeting">Hello, welcome back please login</h1>            
+        </div>
+    )
+}
